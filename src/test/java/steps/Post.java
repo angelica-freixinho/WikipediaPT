db08c1da675059dcee05393adfe4264f0dc661d7
Angel <angelicafn@gmail.com>
2021-04-05 18:29:53 -0300

    Concluimos os relatorios, o Runner e criamos a task smokeTest

--- src/test/java/steps/Post.java
+++ src/test/java/steps/Post.java
@@ -1,5 +1,8 @@
 package steps;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
@@ -16,19 +19,39 @@ public class Post extends Base {
     public Post(Base base) {
         this.base = base;
     }
+//
+//    @Dado("^que acesso a Wikipedia em Portugues$")
+//    public void queAcessoAWikipediaEmPortugues() {
+//        base.driver.get(base.url); // Abre o navegador no site alvo (extendendo da base)
+//    }
+//
+//    @Quando("^pesquiso por \"([^\"]*)\"$")
+//    public void pesquisoPor(String produto){
+//        base.driver.findElement(By.id("searchInput")).sendKeys(produto + Keys.ENTER);
+//
+//    }
+//
+//    @Entao("^exibe a expressao \"([^\"]*)\" no titulo da guia$")
+//    public void exibeAExpressaoNoTituloDaGuia(String produto){
+//        WebDriverWait espera = new WebDriverWait(base.driver, 30); // espera (a mesma que o thread sleep)
+//        espera.until(ExpectedConditions.titleIs("Resultados da pesquisa de \"" +
+//                produto + "\" – Wikipédia, a enciclopédia livre"));
+//        System.out.println("titulo: " + base.driver.getTitle());
+//        assertTrue(base.driver.getTitle().contains(produto));
+//    }
 
-    @Dado("^que acesso a Wikipedia em Portugues$")
+    @Given("^que acesso a Wikipedia em Portugues$")
     public void queAcessoAWikipediaEmPortugues() {
         base.driver.get(base.url); // Abre o navegador no site alvo (extendendo da base)
     }
 
-    @Quando("^pesquiso por \"([^\"]*)\"$")
+    @When("^pesquiso por \"([^\"]*)\"$")
     public void pesquisoPor(String produto){
         base.driver.findElement(By.id("searchInput")).sendKeys(produto + Keys.ENTER);
 
     }
 
-    @Entao("^exibe a expressao \"([^\"]*)\" no titulo da guia$")
+    @Then("^exibe a expressao \"([^\"]*)\" no titulo da guia$")
     public void exibeAExpressaoNoTituloDaGuia(String produto){
         WebDriverWait espera = new WebDriverWait(base.driver, 30); // espera (a mesma que o thread sleep)
         espera.until(ExpectedConditions.titleIs("Resultados da pesquisa de \"" +
@@ -36,4 +59,6 @@ public class Post extends Base {
         System.out.println("titulo: " + base.driver.getTitle());
         assertTrue(base.driver.getTitle().contains(produto));
     }
+
+
 }
